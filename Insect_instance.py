f6203d00d26a83a1746cbd674ea9df3c97580d07
Jaime Suarez <jast1978@gmail.com>
2023-02-09 12:52:13 -0600

    Insects working

---
+++ Insect_instance.py
@@ -0,0 +1,10 @@
+# Create a python program that will create an instance of the insect class and print out how
+# many miles the insect can fly.
+
+import Insects as ins
+
+my_insect = ins.Insect()
+
+my_insect.length_of_flight()
+
+print("The total flight is:", my_insect.land())
